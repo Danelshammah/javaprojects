6e2e3c2615d19d4909959c696643f38cb5e0d492
Shammy <danelshammah@gmail.com>
2021-01-11 08:54:21 +0100

    delete junk

---
+++ projav/src/projav/projav.java
@@ -0,0 +1,29 @@
+package projav;
+
+import java.util.Scanner;
+
+public class projav {
+
+	public static void main(String[] args) {//method
+		System.out.println("Enter your name");
+		
+		//Scanner in = new Scanner(System.in);
+       // String s = in.nextLine();
+        
+       
+        
+        int i = 0; //initialization
+        do {
+        	System.out.println("i is:"+i);
+        	i++;//update
+        } 
+        while(i<10);//condition
+        	
+        	
+       
+        
+		
+
+	}
+  //String x = new String ("Hello"); //property
+}
